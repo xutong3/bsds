2bd4c21accf4fc341b49d2489dd383caafb528b1
杨进 <17612150729@163.com>
2018-07-14 13:34:55 +0800

    LOG修改

--- src/main/java/cc/mrbird/common/util/MD5Utils.java
+++ src/main/java/cc/mrbird/common/util/MD5Utils.java
@@ -4,7 +4,7 @@ import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.util.ByteSource;
 
 public class MD5Utils {
-	private static final String SALT = "mrbird";
+	private static final String SALT = "bsds";
 
 	private static final String ALGORITH_NAME = "md5";
 
@@ -22,7 +22,7 @@ public class MD5Utils {
 	}
 	public static void main(String[] args) {
 		
-		System.out.println(MD5Utils.encrypt("mrbird", "123456"));
+		System.out.println(MD5Utils.encrypt("bsds", "123456"));
 	}
 
 }
